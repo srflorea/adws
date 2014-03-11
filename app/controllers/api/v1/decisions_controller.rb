2b99164e7a47ab0763e167f8175659dc149b5ec0
Razvan Florea <razvan.florea91@gmail.com>
2014-03-11 23:17:31 +0100

    Create method added. Still a test in red.

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::DecisionsController < Api::V1::BaseController
 	def index
 		respond_with(Decision.all)
 	end
+
+	def create
+		decision = Decision.new(decision_params)
+		if decision.save
+			respond_with(decision, :location => api_v1_decision_path(decision))
+		else
+			respond_with(decision)
+		end
+	end
+
+	private
+
+		def decision_params
+			params.require(:decision).permit(:name, :description)
+		end
 end
--- spec/api/v1/decisions_spec.rb
+++ spec/api/v1/decisions_spec.rb
@@ -18,4 +18,29 @@ describe "api/v1/decisions", :type => :api do
 			end.should be_true
 		end
 	end
+
+	context "creating a decision" do
+		let(:url) { "/api/v1/decisions" }
+		it "successful JSON" do
+			post "#{url}.json", :decision => {
+									:name => "decision2",
+									:description => "desc"
+									}
+			decision = Decision.find_by_name!("decision2")
+			route = "/api/v1/decisions/#{decision.id}"
+			last_response.status.should eql(201)
+			last_response.headers["Location"].should eql(route)
+			last_response.body.should eql(decision.to_json)
+		end
+
+		it "unsuccessful JSON" do
+			post "#{url}.json", :decision => { }
+			puts last_response.status
+			last_response.status.should eql(422)
+			errors = {"errors" => {
+						"name" => ["can't be blank"]
+						}}.to_json
+			last_response.body.should eql(errors)
+		end
+	end
 end
