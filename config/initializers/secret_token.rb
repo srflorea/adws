8957c6d1e2a4ab7347b832a1fd9693f1b40792b6
Razvan Florea <razvan.florea91@gmail.com>
2014-03-11 19:45:55 +0100

    Generate architecture decisions web service

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adws::Application.config.secret_key_base = '2d73130dea909c0e7aded8bafaa167e4b051ebd8705e44ed84932acbd4ecfbd12fe9a56448466e3e8d2acad9438bebf6d34cd4f5bcaf35bf8ed74032bcdcbda1'
